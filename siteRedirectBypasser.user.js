aaecbd1b310d56c512b4a7079a2ea52c0c57e7a7
Nihaal Sangha <18350092+OrangutanGaming@users.noreply.github.com>
2018-02-22 13:01:24 +0000

    Added creativecow.net/interstitial.php
    Converted match tags to included, and added www. to the previous regexs

--- siteRedirectBypasser.user.js
+++ siteRedirectBypasser.user.js
@@ -1,26 +1,28 @@
 // ==UserScript==
 // @name         Site Redirect Bypasser
 // @namespace    https://github.com/OrangutanGaming/siteRedirectBypasser
-// @version      0.1.2
+// @version      0.1.3
 // @description  Bypasses redirect sites
 // @author       OrangutanGaming
 // @run-at       document-start
 // @updateURL	 https://raw.githubusercontent.com/OrangutanGaming/siteRedirectBypasser/master/siteRedirectBypasser.user.js
 // @downloadURL  https://raw.githubusercontent.com/OrangutanGaming/siteRedirectBypasser/master/siteRedirectBypasser.user.js
 // @supportURL   https://github.com/OrangutanGaming/siteRedirectBypasser/issues
-// @match        *bluemediafiles.com/creatinglinks8qJG9LfyFidlaldiwli1kTUSkSn82FylsejFCipVsahU2r2FXfgX2LgYHme3*
-// @match        *steamcommunity.com/linkfilter/?url=*
-// @match        *rover.ebay.*/rover/0/0/0?mpre=*
-// @match        *anon.to/?*
+// @include      *bluemediafiles.com/creatinglinks8qJG9LfyFidlaldiwli1kTUSkSn82FylsejFCipVsahU2r2FXfgX2LgYHme3*
+// @include      *steamcommunity.com/linkfilter/?url=*
+// @include      *rover.ebay.*/rover/0/0/0?mpre=*
+// @include      *anon.to/?*
+// @include      *creativecow.net/interstitial.php?url=*
 // ==/UserScript==
 
 var url = content.document.location;
 
 var regexs = [
-    [/https{0,1}:\/\/bluemediafiles\.com\/creatinglinks8qJG9LfyFidlaldiwli1kTUSkSn82FylsejFCipVsahU2r2FXfgX2LgYHme3\?xurl=(.+)/g, 'http'],
-    [/https{0,1}:\/\/steamcommunity\.com\/linkfilter\/\?url=(.+)/g, ''],
-    [/https{0,1}:\/\/rover\.ebay\.[a-z.]+\/rover\/0\/0\/0\?mpre=(.+)/g, ''],
-    [/https{0,1}:\/\/anon\.to\/\?(.+)/g, '']
+    [/https{0,1}:\/\/(?:www\.){0,1}bluemediafiles\.com\/creatinglinks8qJG9LfyFidlaldiwli1kTUSkSn82FylsejFCipVsahU2r2FXfgX2LgYHme3\?xurl=(.+)/g, 'http'],
+    [/https{0,1}:\/\/(?:www\.){0,1}steamcommunity\.com\/linkfilter\/\?url=(.+)/g, ''],
+    [/https{0,1}:\/\/(?:www\.){0,1}rover\.ebay\.[a-z.]+\/rover\/0\/0\/0\?mpre=(.+)/g, ''],
+    [/https{0,1}:\/\/(?:www\.){0,1}anon\.to\/\?(.+)/g, ''],
+    [/https{0,1}:\/\/(?:www\.){0,1}creativecow\.net\/interstitial\.php\?url=(.+)&id=\d/g, '']
 ];
 
 for (i = 0; i < regexs.length; i++) {
