b8332bf948a4e396dd75b96ab4adca26472252af
Nihaal Sangha <18350092+OrangutanGaming@users.noreply.github.com>
2018-08-26 21:12:53 +0100

    0.1.9

--- siteRedirectBypasser.user.js
+++ siteRedirectBypasser.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Site Redirect Bypasser
 // @namespace    https://github.com/OrangutanGaming/siteRedirectBypasser
-// @version      0.1.8
+// @version      0.1.9
 // @description  Bypasses redirect sites
 // @author       OrangutanGaming (Nihaal Sangha)
 // @run-at       document-start
@@ -9,13 +9,14 @@
 // @downloadURL  https://raw.githubusercontent.com/OrangutanGaming/siteRedirectBypasser/master/siteRedirectBypasser.user.js
 // @supportURL   https://github.com/OrangutanGaming/siteRedirectBypasser/issues
 // @include      *bluemediafiles.com/creatinglinks8qJG9LfyFidlaldiwli1kTUSkSn82FylsejFCipVsahU2r2FXfgX2LgYHme3*
-// @include      *steamcommunity.com/linkfilter/?url=*
+// @include      *steamcommunity.com/linkfilter*
 // @include      *rover.ebay.*/rover/0/0/0?mpre=*
-// @include      *anon.to/?*
-// @include      *creativecow.net/interstitial.php?url=*
-// @include      *guildwars2.com/home/leaving?target=*
-// @include      *checktinyurl.com/redirect?url=*
-// @include      *t.umblr.com/redirect?z=*
+// @include      *anon.to/*
+// @include      *creativecow.net/interstitial.php*
+// @include      *guildwars2.com/home/leaving*
+// @include      *checktinyurl.com/redirect*
+// @include      *t.umblr.com/redirect*
+// @include      *plus.url.google.com/url*
 // ==/UserScript==
 
 'use strict';
@@ -23,14 +24,15 @@
 var url = document.location;
 
 var regexs = [
-    [/https{0,1}:\/\/(?:www\.){0,1}bluemediafiles\.com\/creatinglinks8qJG9LfyFidlaldiwli1kTUSkSn82FylsejFCipVsahU2r2FXfgX2LgYHme3\?xurl=(.+)/g, 'http'],
-    [/https{0,1}:\/\/(?:www\.){0,1}steamcommunity\.com\/linkfilter\/\?url=(.+)/g, ''],
-    [/https{0,1}:\/\/(?:www\.){0,1}rover\.ebay\.[a-z.]+\/rover\/0\/0\/0\?mpre=(.+)/g, ''],
-    [/https{0,1}:\/\/(?:www\.){0,1}anon\.to\/\?(.+)/g, ''],
-    [/https{0,1}:\/\/(?:www\.){0,1}creativecow\.net\/interstitial\.php\?url=(.+)&id=\d/g, ''],
-    [/https{0,1}:\/\/(?:en|fr|es|de)-forum\.guildwars2\.com\/home\/leaving\?target=(.+)/g, ''],
-    [/https{0,1}:\/\/(?:www\.){0,1}checktinyurl\.com\/redirect\?url=(.+)/g, ''],
-    [/https{0,1}:\/\/(?:www\.){0,1}t\.umblr\.com\/redirect\?z=(.*?)&.+/g, '']
+    [/^https{0,1}:\/\/(?:www\.){0,1}bluemediafiles\.com\/creatinglinks8qJG9LfyFidlaldiwli1kTUSkSn82FylsejFCipVsahU2r2FXfgX2LgYHme3\?(?:[^&/]*?=[^&/]*?&?)*?xurl=([^&/]+)/g, 'http'],
+    [/^https{0,1}:\/\/(?:www\.){0,1}steamcommunity\.com\/linkfilter\/\?(?:[^&/]*?=[^&/]*?&?)*?url=([^&/]+)/g, ''],
+    [/^https{0,1}:\/\/(?:www\.){0,1}rover\.ebay\.[a-z.]+\/rover\/0\/0\/0\?(?:[^&/]*?=[^&/]*?&?)*?mpre=([^&/]+)/g, ''],
+    [/^https{0,1}:\/\/(?:www\.){0,1}anon\.to\/\?(?:[^&/]*?=[^&/]*?&?)*?([^&/]+)/g, ''],
+    [/^https{0,1}:\/\/(?:www\.){0,1}creativecow\.net\/interstitial\.php\?(?:[^&/]*?=[^&/]*?&?)*?url=([^&/]+)&id=\d/g, ''],
+    [/^https{0,1}:\/\/(?:en|fr|es|de)-forum\.guildwars2\.com\/home\/leaving\?(?:[^&/]*?=[^&/]*?&?)*?target=([^&/]+)/g, ''],
+    [/^https{0,1}:\/\/(?:www\.){0,1}checktinyurl\.com\/redirect\?(?:[^&/]*?=[^&/]*?&?)*?url=([^&/]+)/g, ''],
+    [/^https{0,1}:\/\/(?:www\.){0,1}t\.umblr\.com\/redirect\?(?:[^&/]*?=[^&/]*?&?)*?z=([^&/]+)&.+/g, ''],
+    [/^https{0,1}:\/\/(?:www\.){0,1}plus\.url\.google\.com\/url\?(?:[^&/]*?=[^&/]*?&?)*?url=([^&/]+)/g, '']
 ];
 
 for (var i = 0; i < regexs.length; i++) {
